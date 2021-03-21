d6d5bc3f395010d741abfb5bff63faffe265267a
Adam Plencler <aplencler@gmail.com>
2021-03-21 18:08:00 +0100

    add jpackage note

--- src/main/java/swingVersion/Main.java
+++ src/main/java/swingVersion/Main.java
@@ -6,5 +6,7 @@ class Main {
 
     public static void main(String[] arguments) throws IOException {
         new SwingWindow();
+
+        //jpackage --input target/ --name Kalkulator_szyn_Solarspot --main-jar KalkulatorSolarspot-0.9-SNAPSHOT.jar --win-shortcut --win-menu --icon src\main\resources\solar.ico
     }
 }
